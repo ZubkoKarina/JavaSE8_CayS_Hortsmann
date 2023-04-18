b350aafe18bd053630886c407f7459e370784da9
Karina Zubko <karinazubko2017@gmail.com>
2023-04-18 23:07:08 +0200

    ch01.sec01

--- ch01/sec01/Task_1.java
+++ ch01/sec01/Task_1.java
@@ -1,7 +1,7 @@
 package ch01.sec01;
 import java.util.Scanner;
 
-public class Task_1 {
+public class Task_1{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number:");
---
+++ ch01/sec01/Task_2.java
@@ -0,0 +1,12 @@
+package ch01.sec01;
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter int numbers:");
+        int num = in.nextInt();
+        System.out.println(num);
+
+    } 
+}
