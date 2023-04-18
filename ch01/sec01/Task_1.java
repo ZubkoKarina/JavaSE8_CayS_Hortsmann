b33ee54ea29e5347d0ca6654e9d28839f01cd299
Karina Zubko <karinazubko2017@gmail.com>
2023-04-18 22:59:24 +0200

    ch01.sec01

--- ch01/sec01/Task_1.java
+++ ch01/sec01/Task_1.java
@@ -1,7 +1,16 @@
 package ch01.sec01;
+import java.util.Scanner;
 
-public class Task_1{
+public class Task_1 {
     public static void main(String[] args){
-        Ssytem.out.println("a");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int num = in.nextInt();
+        String binary = Integer.toBinaryString(num);
+        String octal = Integer.toOctalString(num);
+        String hexadecimal = Integer.toHexString(num);
+        double inverse = 1.0/num;
+        String hexInverse = Double.toHexString(inverse);
+        System.out.println(hexInverse);
     }
 }
