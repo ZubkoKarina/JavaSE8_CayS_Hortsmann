a113b54310d4223136fe94a9d8d6392b43e3d02e
Karina Zubko <karinazubko2017@gmail.com>
2023-04-19 19:01:00 +0200

    ch01.sec01

---
+++ ch01/sec01/Task_5.java
@@ -0,0 +1,10 @@
+package ch01.sec01;
+
+public class Task_5{
+    public static void main(String[] args){
+        double d = 12341341234341234.5;
+        int i = (int)d;
+        System.out.println(i);
+        /*переполнение int - непредсказуемый результат */
+    }
+}
