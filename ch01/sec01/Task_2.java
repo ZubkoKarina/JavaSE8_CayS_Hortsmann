60094779e8fb0884ee48b3c6e530533fd158b0be
Karina Zubko <karinazubko2017@gmail.com>
2023-04-19 17:46:12 +0200

    ch01.sec01

--- ch01/sec01/Task_2.java
+++ ch01/sec01/Task_2.java
@@ -6,7 +6,12 @@ public class Task_2 {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter int numbers:");
         int num = in.nextInt();
-        System.out.println(num);
-
+        int normalizedNumber = num%360;
+        if (normalizedNumber<0){
+            normalizedNumber *= (-1);
+        }
+        System.out.println(normalizedNumber);
+        normalizedNumber = Math.floorMod(num, 360);
+        System.out.println(normalizedNumber);
     } 
 }
