88babcb7022cf62d1f09166b728a1503502ce32d
Karina Zubko <karinazubko2017@gmail.com>
2023-04-22 21:17:32 +0200

    ch02/sec01

---
+++ ch02/sec01/Task_2.java
@@ -0,0 +1,10 @@
+package ch02.sec01;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        /* 
+         * Метод nextInt() из класса Scanner является методом доступа
+         * Метод nextInt() из класса Random также является методом доступа
+         */
+    }
+}
